9c24767bba9b750671b44bde6677ac6ba5b0b9bd
Hermeson Do Vale Beserra <72263429+hermeson883@users.noreply.github.com>
2024-04-15 23:07:18 +0000

    quebrando tudo

--- Showcase.py
+++ Showcase.py
@@ -7,7 +7,7 @@ st.title("*RECAP* 2023 :rewind:")
 st.markdown("""
     ### | Relembrando os melhores momentos de 2023
 """)
-
+dsgfhfgjdfgbgfnb
 st.markdown("""
     Ei :red[**você**!] Vem me acompanhar em alguns dos registros que fiz ao longo desse ano tanto como professor e quanto aluno😅.  
     Se liga só nas Ferramentas e habilidades que desenvolvi ao longo do ano e que tive a oportunidade de compartilhar com os [alunos](Infinity♾) e amigos que fiz esse ano.
